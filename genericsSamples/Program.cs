df95bf3f6357b92ffdc4c392cbed08ecd66abf6d
Marcos Ribeiro <marcos@azacessorios.com.br>
2023-01-25 14:09:33 -0300

    Problema motivador 1 (reuso)

--- genericsSamples/Program.cs
+++ genericsSamples/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using genericsSamples;
+
+PrintService ps = new PrintService();
+
+Console.Write("How many values? ");
+int n = int.Parse(Console.ReadLine());
+for(int i = 0; i < n; i++)
+{
+    int x = int.Parse(Console.ReadLine());
+    ps.AddValue(x);
+}
+ps.Print();
+Console.Write("First: " + ps.First);
